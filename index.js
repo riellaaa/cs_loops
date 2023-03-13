2589baee940d365dc0b715c8252dd8a0d8495db9
riellaaa <127709698+riellaaa@users.noreply.github.com>
2023-03-13 11:17:25 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+//while
+var i = 0;
+
+while(i < 60){
+	i=i+2;
+	document.writeln(i);
+}
+//do while
+var s = 0;
+do{
+	s=s+3;
+	document.writeln(s);
+}
+while(s<60)
+
+//for
+var a = 0;
+for(a=5; a<51; a=a+5){
+	document.writeln(a);
+}
